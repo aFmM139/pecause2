674426e8c6c0e662740646520ccd22b1e5ab591a
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-10-01 16:16:17 +0000

    💄ui(index): Asignar color de fondo para la aplicacion

--- app/index.tsx
+++ app/index.tsx
@@ -3,15 +3,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-    style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-    >
-      <View className="bg-blue-500 w-screen h-screen">
+    <View className="bg-blue-500 w-screen h-screen">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View className="bg-[#026A75] w-screen h-screen">
+        <Text className="">Edit app/index.tsx to edit this screen.</Text>
       </View>
     </View>
   );
