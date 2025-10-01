40fdec4f41fe0bff6ca6d691352e895369d16801
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-10-01 16:13:08 +0000

    ✨feat(app): Instalacion de nativeWind y estilos de prueba - Agregar metro.config, babel.config con el comando npx expo customize - Crear hoja de estilos global.css - Crear tailwind.config con el comando npx tailwindcss init - (index) Añadir estilos de prueba

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
+import "@/global.css";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
     >
+      <View className="bg-blue-500 w-screen h-screen">
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      </View>
     </View>
   );
 }
