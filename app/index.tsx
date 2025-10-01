1e82a5ddf2c9da50c49b03eac40bbdd06cd9eb3a
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-10-01 16:51:17 +0000

    ✨ feat(ui/CustomText): Crear un CustomText que se adapte a diferentes escenarios - props ->     variant para tamaño del texto,     dark para el color y     children ReactNode para el contenido - Insertar en (index) para pruebas del componente

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
+import CustomText from "@/Components/CustomText";
 import "@/global.css";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="bg-blue-500 w-screen h-screen">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <View className="bg-[#026A75] w-screen h-screen">
-        <Text className="">Edit app/index.tsx to edit this screen.</Text>
-      </View>
+    <View className="bg-[#026A75] w-screen h-screen">
+      <Text className="">Edit app/index.tsx to edit this screen.</Text>
+      <CustomText variant="large" dark={false}>
+        Hello World
+      </CustomText>
     </View>
   );
 }
