82069cf6d5e5562b1dca28cf8e53cd32c006045d
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-10-13 03:28:40 +0000

    ✨ feat(SettingsScreens):añadir navegación entre paginas

---
+++ app/(home)/SettingsScreens.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+import { View,Text } from 'react-native'
+import { Link } from "expo-router";
+
+  export default function SettingsScreens(){
+  return (
+    <>
+    <Stack.Screen options={{title:"Cuchisito"}}/>
+    <View>
+    <Text>Cuchisito tu papá</Text>
+    <Link href="/(home)/HomeScreen">hola</Link>
+    </View>
+    </>
+  )
+}
