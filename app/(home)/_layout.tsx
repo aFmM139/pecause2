353ab42b9ccb825e7e750386316ea235999b33cc
andyfajardo13092008 <andyfajardo13092008@gmail.com>
2025-10-13 03:29:23 +0000

    ✨ feat(app/(home)/_layout):añadir configuración stack

---
+++ app/(home)/_layout.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const _layout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name="index" options={{
+            title:"Home"
+        }}/>
+        <Stack.Screen name="SettingsScreens" options={{
+            title:"Configuraciones"
+        }}/>
+    </Stack>
+  )
+}
+
+export default _layout
